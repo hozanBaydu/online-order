b2a0181da68275492119f3c8d7052b53c5be2e5a
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-10-23 07:46:16 +0300

    adding RxRoom class

--- library/src/main/java/com/hozanbaydu/lib/RxRoom.kt
+++ library/src/main/java/com/hozanbaydu/lib/RxRoom.kt
@@ -6,10 +6,12 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
+
 class RxRoom {
     private var compositeDisposable= CompositeDisposable()
     private lateinit var dp:OnlineOrderFoodsDataBase
     private lateinit var foodDao:OnlineOrderFoodsDao
+
 
      fun onlineOrderinsert (context: Context, onlineOrdersFoodsModel:OnlineOrdersFoodsModel){
 
@@ -39,25 +41,5 @@ class RxRoom {
         )
     }
 
-     fun onlineOrdergetAll(context: Context){
-        dp= Room.databaseBuilder(context,OnlineOrderFoodsDataBase::class.java,"OnlineOrdersFoodsModel").build()
-        foodDao=dp.OnlineOrderFoodsDao()
-        compositeDisposable.add(
 
-            foodDao.getAll()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(this::handleResponse)
-        )
-
-    }
-
-     fun handleResponse (list: MutableList<OnlineOrdersFoodsModel>){
-
-        println(list[0].name)
-
-
-
-
-    }
 }

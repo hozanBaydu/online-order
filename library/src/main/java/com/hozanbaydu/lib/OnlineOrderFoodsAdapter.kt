7db74fc6684f5d6828dcc52de17b82f017c14321
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-10-24 03:16:47 +0300

    adapter changing

--- library/src/main/java/com/hozanbaydu/lib/OnlineOrderFoodsAdapter.kt
+++ library/src/main/java/com/hozanbaydu/lib/OnlineOrderFoodsAdapter.kt
@@ -9,52 +9,30 @@ import kotlinx.android.synthetic.main.onlineorderrecyclerrow.view.*
 
 class OnlineOrderFoodsAdapter (val artList:MutableList<OnlineOrdersFoodsModel>): RecyclerView.Adapter<OnlineOrderFoodsAdapter.FoodsHolder>(){
 
-
-
     class FoodsHolder(view: View ) : RecyclerView.ViewHolder(view) {
-
     }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodsHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.onlineorderrecyclerrow, parent, false)
         return FoodsHolder(view)
-
-
     }
-
     override fun onBindViewHolder(holder: FoodsHolder, position: Int) {
         holder.itemView.textView.text=artList.get(position).name
          setImageViewResource(holder.itemView.imageView,artList.get(position).image)
 
-
-
-
-
-
         holder.itemView.imageButton3.setOnClickListener {
-
-
             val name =artList.get(position).name
             val itemPosition=position.toString()
             Toast.makeText(holder.itemView.context,name+" Sepetten çıkarıldı.", Toast.LENGTH_SHORT).show()
 
-
-
-
         }
-
         holder.itemView.imageButton2.setOnClickListener {
             val a =artList.get(position).name
             val itemPosition=position.toString()
 
             Toast.makeText(holder.itemView.context,a+" Sepete eklendi.", Toast.LENGTH_SHORT).show()
 
-
-
         }
-
     }
-
     override fun getItemCount(): Int {
         return artList.size
     }
